85b4615f86d9bd66a70e30d6b44fa6e2c1923fa1
Khalid <67862481+khalidbayan@users.noreply.github.com>
2023-01-03 14:03:51 +0200

    Create day-one.js
    Finsihed Day One of Learning JS

---
+++ Day-One/day-one.js
@@ -0,0 +1,61 @@
+// Write a single line comment which says, comments can make code readable
+// Comments can make code readable 
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* 
+comments can make code readable,
+easy to reuse,
+and informative
+
+*/
+
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let varOne = " a string"
+let isBool = true
+let undefind_value = undefined
+let null_variable = null
+
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof varOne)
+console.log(typeof isBool)
+console.log(typeof undefind_value)
+console.log(typeof null_variable)
+
+
+
+
+
+// Declare 2 variables without assigning values
+
+let my_name;
+let my_age;
+
+// Declare 2 variables with assigned values
+
+let my_country = "Lebanon"
+let my_status = "Single"
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = "Khalid"
+let lastName = "Bayan"
+let age = 24
+let country = "Lebanon"
+let martial_status = "Single"
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let first_name = "Ali" , last_name = "Bayan" , his_age = 34 , his_martial_status = "single"
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge =  24
+let yourAge = 34
+
+console.log(myAge , yourAge)
